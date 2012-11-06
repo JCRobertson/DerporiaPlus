378ab118d5c8470931cb4a5c4cf15a5bec2c4d72
James Dressel <ubergeek@jamesdressel.com>
2012-11-05 21:31:08 -0500

    Add template file

---
+++ derporia.java
@@ -0,0 +1,41 @@
+package jdressel;
+// James Dressel and James Robertson
+// Based on the Hello.java example
+// Import Servlet Libraries
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+// Import Java Libraries
+import java.io.*;
+
+
+public class Hello extends HttpServlet
+{
+
+   public void doGet  (HttpServletRequest req, HttpServletResponse res)
+          throws ServletException, IOException
+   {
+
+        res.setContentType ("text/html");
+        PrintWriter out = res.getWriter ();
+
+        out.println ("<HTML>");
+        out.println ("<HEAD>");
+        out.println ("<TITLE>A simple servlet program</TITLE>");
+        out.println ("</HEAD>");
+
+        out.println ("<BODY>");
+        out.println ("<CENTER>");
+
+        out.println (" <B>Hello!</B><BR> <!--  English -->");
+  
+
+        out.println ("</CENTER>");
+        out.println ("</BODY>");
+
+        out.println ("</HTML>");
+
+        out.close ();
+
+    }
+}
